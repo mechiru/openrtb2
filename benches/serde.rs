53b878e813e81e8414e04cf00d407ae80210eda1
mechiru <46036584+mechiru@users.noreply.github.com>
2020-11-11 03:07:34 +0900

    Refactor code

--- benches/serde.rs
+++ benches/serde.rs
@@ -8,11 +8,7 @@ use openrtb2::BidRequest;
 
 const BID_REQUEST: &str = include_str!("../tests/json/6.3.4_video.json");
 
-fn deserialize() -> BidRequest<'static> {
-    serde_json::from_str::<BidRequest>(BID_REQUEST).unwrap()
-}
-
 #[bench]
 fn bench_deserialize(b: &mut Bencher) {
-    b.iter(|| deserialize());
+    b.iter(|| serde_json::from_str::<BidRequest>(BID_REQUEST).unwrap());
 }
